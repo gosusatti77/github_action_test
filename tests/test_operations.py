5c29b6b09b5a94ea1cbac8d5e8fe88b0725fc260
Satish Gosu <152600120+gosusatti77@users.noreply.github.com>
2025-05-26 13:14:26 +0530

    Update test_operations.py

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -2,7 +2,7 @@ from src.math_operations import add, sub
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
-    assert add(5,3) == 0
+    assert add(5,3) == 8
 
 def test_sub():
     assert sub(2, 3) == -1
